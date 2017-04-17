47161157bcf2990ba5c95c0b11c656bc027c806f
Artem Poddybnyy <artpoddybnyy@gmail.com>
2017-04-17 18:43:14 +0300

    try add popup window for edit component

--- target/classes/import.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO  bookmarks (title, link, date_creation)VALUES
-  ( 'angular2-springboot-starter', 'https://github.com/artpoddybnyy/angular2-springboot-starter', CURRENT_DATE);
-
-INSERT INTO  bookmarks (title, link, date_creation)VALUES
-  ( 'java', 'http://info.javarush.ru/translation/2014/02/12/Java-Core-Вопросы-к-собеседованию-ч-1.html', CURRENT_DATE);
-
-
-
-
